1a792922b40f3211fb9d5b0386941c76e58ecd5f
Kostiantyn Makarovskyi <makarovskiy.k@gmail.com>
2023-03-18 18:09:09 +0200

    test commit 1

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+
+import {
+  useFonts as useMontserrat,
+  Montserrat_400Regular,
+} from "@expo-google-fonts/montserrat";
+
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+
+export default function App() {
+  const [MontserratLoaded] = useMontserrat({
+    Montserrat_400Regular,
+  });
+
+  if (!MontserratLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
